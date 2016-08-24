8019be0d096ba64f1f40442943691d3be17363ee
Sean Wu <slwu89@berkeley.edu>
2016-08-23 22:42:12 -0700

    C++ version of pnorm and qnorm

--- gibbs_tmvtgaussian.cpp
+++ gibbs_tmvtgaussian.cpp
@@ -7,7 +7,25 @@ using namespace Rcpp;
  */
 
 
-// CDF_norm returns the cumulative distribution function of the standard normal (N(0,1)) evaluated at x (value)
+
+// r8poly_value_horner evaluates a polynomial using Horner's method
+double r8poly_value_horner(int m, double c[], double x){
+  
+  double value;
+  value = c[m];
+ 
+  for(int i = m - 1; 0 <= i; i--){
+    value = value * x + c[i];
+  }
+  
+  return value;
+}
+
+
+/*
+ * CDF_norm returns the cumulative distribution function of the standard normal (N(0,1)) evaluated at x (value);
+ * this function replicates the functionality of pnorm from base R
+ */
 // [[Rcpp::export]]
 double CDF_norm(double x){
   double a1 = 0.398942280444;
@@ -53,59 +71,88 @@ double CDF_norm(double x){
   return cdf;
 }
 
-// invCDF_norm returns the inverse cumulative distribution function of the standard normal (N(0,1)) evaluated at x (probability)
-// double invCDF_norm()
-// 
-// 
-// double AffxStatistics::norminv(double p)
-// {
-//   const double a[6] = {
-//     -3.969683028665376e+01,  2.209460984245205e+02,
-//     -2.759285104469687e+02,  1.383577518672690e+02,
-//     -3.066479806614716e+01,  2.506628277459239e+00
-//   };
-//   const double b[5] = {
-//     -5.447609879822406e+01,  1.615858368580409e+02,
-//     -1.556989798598866e+02,  6.680131188771972e+01,
-//     -1.328068155288572e+01
-//   };
-//   const double c[6] = {
-//     -7.784894002430293e-03, -3.223964580411365e-01,
-//     -2.400758277161838e+00, -2.549732539343734e+00,
-//     4.374664141464968e+00,  2.938163982698783e+00
-//   };
-//   const double d[4] = {
-//     7.784695709041462e-03,  3.224671290700398e-01,
-//     2.445134137142996e+00,  3.754408661907416e+00
-//   };
-//   
-//   register double q, t, u;
-//   
-//   if ((p != p) || p > 1.0 || p < 0.0)
-//     return NaN;
-//   if (p == 0.0)
-//     return NaN;
-//   if (p == 1.0)
-//     return  NaN;
-//   q = min(p,1-p);
-//   if (q > 0.02425) {
-//     /* Rational approximation for central region. */
-//     u = q-0.5;
-//     t = u*u;
-//     u = u*(((((a[0]*t+a[1])*t+a[2])*t+a[3])*t+a[4])*t+a[5])
-//       /(((((b[0]*t+b[1])*t+b[2])*t+b[3])*t+b[4])*t+1);
-//   } else {
-//     /* Rational approximation for tail region. */
-//     t = sqrt(-2*log(q));
-//     u = (((((c[0]*t+c[1])*t+c[2])*t+c[3])*t+c[4])*t+c[5])
-//       /((((d[0]*t+d[1])*t+d[2])*t+d[3])*t+1);
-//   }
-//   /* The relative error of the approximation has absolute value less
-//    than 1.15e-9.  One iteration of Halley's rational method (third
-//    order) gives full machine precision... */
-//   t = normcdf(u)-q;    /* error */
-//   t = t*sqrt(2*PI)*exp(u*u/2);   /* f(u)/df(u) */
-//   u = u-t/(1+u*t/2);     /* Halley's method */
-//   
-//   return (p > 0.5 ? -u : u);
-// };
+
+/*
+ * invCDF_norm returns the inverse cumulative distribution function of the standard normal (N(0,1)) evaluated at x (probability);
+ * this function replicates the functionality of qnorm from base R
+ */
+// [[Rcpp::export]]
+double invCDF_norm(double p){
+  double a[8] = {
+    3.3871328727963666080,     1.3314166789178437745E+2,
+    1.9715909503065514427E+3,  1.3731693765509461125E+4,
+    4.5921953931549871457E+4,  6.7265770927008700853E+4,
+    3.3430575583588128105E+4,  2.5090809287301226727E+3 };
+  double b[8] = {
+    1.0,                       4.2313330701600911252E+1,
+    6.8718700749205790830E+2,  5.3941960214247511077E+3,
+    2.1213794301586595867E+4,  3.9307895800092710610E+4,
+    2.8729085735721942674E+4,  5.2264952788528545610E+3 };
+  double c[8] = {
+    1.42343711074968357734,     4.63033784615654529590,
+    5.76949722146069140550,     3.64784832476320460504,
+    1.27045825245236838258,     2.41780725177450611770E-1,
+    2.27238449892691845833E-2,  7.74545014278341407640E-4 };
+  double const1 = 0.180625;
+  double const2 = 1.6;
+  double d[8] = {
+    1.0,                        2.05319162663775882187,
+    1.67638483018380384940,     6.89767334985100004550E-1,
+    1.48103976427480074590E-1,  1.51986665636164571966E-2,
+    5.47593808499534494600E-4,  1.05075007164441684324E-9 };
+  double e[8] = {
+    6.65790464350110377720,     5.46378491116411436990,
+    1.78482653991729133580,     2.96560571828504891230E-1,
+    2.65321895265761230930E-2,  1.24266094738807843860E-3,
+    2.71155556874348757815E-5,  2.01033439929228813265E-7 };
+  double f[8] = {
+    1.0,                        5.99832206555887937690E-1,
+    1.36929880922735805310E-1,  1.48753612908506148525E-2,
+    7.86869131145613259100E-4,  1.84631831751005468180E-5,
+    1.42151175831644588870E-7,  2.04426310338993978564E-15 };
+  double q;
+  double r;
+  double split1 = 0.425;
+  double split2 = 5.0;
+  double value;
+  
+  if (p <= 0.0){
+    value = -HUGE_VAL;
+    return value;
+  }
+  if (1.0 <= p){
+    value = HUGE_VAL;
+    return value;
+  }
+
+  q = p - 0.5;
+  
+  if(fabs(q) <= split1){
+    r = const1 - q * q;
+    value = q * r8poly_value_horner(7,a,r) / r8poly_value_horner(7,b,r);
+  } else {
+    if(q < 0.0){
+      r = p;
+    } else {
+      r = 1.0 - p;
+    }
+     if (r <= 0.0){
+      value = HUGE_VAL;
+    } else {
+      r = sqrt ( - log ( r ) );
+      if(r <= split2){
+        r = r - const2;
+        value = r8poly_value_horner(7,c,r) / r8poly_value_horner(7,d,r);
+      } else {
+        r = r - split2;
+        value = r8poly_value_horner(7,e,r) / r8poly_value_horner(7,f,r);
+      }
+    }
+    
+    if (q < 0.0){
+      value = - value;
+    }
+  }
+  
+  return value;
+}
